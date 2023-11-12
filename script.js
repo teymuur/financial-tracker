190779abe3829673947ac60404d62c51686ef6f7
teymuur <teymur_babayev08@yahoo.com>
2023-11-12 12:52:45 +0400

    Added bg photo

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let totalExpenses = 0;
+let totalExpenses = parseFloat(getCookie('totalExpenses')) || 0;
+let salary = parseFloat(getCookie('salary')) || 0;
+
 function addExpense() {
     const category = document.getElementById('category').value;
     const amount = document.getElementById('amount').value;
@@ -9,19 +11,22 @@ function addExpense() {
         return;
     }
 
-    const expenseTable = document.getElementById('expenseTable').getElementsByTagName('tbody')[0];
-    const newRow = expenseTable.insertRow(-1);
-
-    const cell1 = newRow.insertCell(0);
-    const cell2 = newRow.insertCell(1);
-    const cell3 = newRow.insertCell(2);
-
-    cell1.innerHTML = category;
-    cell2.innerHTML = `$${amount}`;
-    cell3.innerHTML = date;
-
+    // Update total expenses
     totalExpenses += parseFloat(amount);
     document.getElementById('totalAmount').textContent = totalExpenses.toFixed(2);
+    setCookie('totalExpenses', totalExpenses);
+
+    // Update remaining money
+    updateRemainingMoney();
+
+    // Save expense to cookie
+    const expenseData = getCookie('expenseData') || '[]';
+    const expenses = JSON.parse(expenseData);
+    expenses.push({ category, amount, date });
+    setCookie('expenseData', JSON.stringify(expenses));
+
+    // Render expense history
+    renderExpenseHistory();
 
     // Clear input fields
     document.getElementById('category').value = '';
@@ -30,21 +35,48 @@ function addExpense() {
 }
 
 function updateSalary() {
-    const salary = document.getElementById('salaryInput').value;
+    salary = parseFloat(document.getElementById('salaryInput').value) || 0;
+    setCookie('salary', salary);
 
-    const remainingMoney = salary - totalExpenses;
-    document.getElementById('remainingAmount').textContent = remainingMoney.toFixed(2);
-    // You can add additional logic here, such as updating a budget based on salary.
+    // Update remaining money
+    updateRemainingMoney();
 
-    // Clear input field
 
 }
+
+function updateRemainingMoney() {
+    const remainingMoney = salary - totalExpenses;
+    document.getElementById('remainingAmount').textContent = remainingMoney.toFixed(2);
+}
+
+function renderExpenseHistory() {
+    const expenseTable = document.getElementById('expenseTable').getElementsByTagName('tbody')[0];
+    const expenseData = getCookie('expenseData') || '[]';
+    const expenses = JSON.parse(expenseData);
+
+    // Clear previous rows
+    expenseTable.innerHTML = '';
+
+    // Render new rows
+    expenses.forEach(expense => {
+        const newRow = expenseTable.insertRow(-1);
+        const cell1 = newRow.insertCell(0);
+        const cell2 = newRow.insertCell(1);
+        const cell3 = newRow.insertCell(2);
+
+        cell1.innerHTML = expense.category;
+        cell2.innerHTML = `$${expense.amount}`;
+        cell3.innerHTML = expense.date;
+    });
+}
+
+
 document.getElementById('salaryInput').addEventListener("input",updateSalary)
 
 
-function setCookie(cname,cvalue,exdays) {
+function setCookie(cname,cvalue) {
     const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    d.setTime(d.getTime() + (365*24*60*60*1000));
     let expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
   }
